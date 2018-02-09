3c3f46db95a13920785cc92234988addc85b3bc9
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-09 13:53:14 +0300

    made an online calculator just for practice in php

--- scripts/calculate.php
+++ scripts/calculate.php
@@ -1,9 +1,32 @@
-<body>
+<body align = "center">
 	Result: <br><br>
 
 	<?php
-		$username = $_POST['username'];
-		echo "Hello, " . $username;
+		$num1 = $_POST["num1"];
+		$num2 = $_POST["num2"];
+		
+		$op = $_POST["op"];
+
+		if ($op == "add")
+		{
+			echo "<h2>" . ($num1 + $num2) . "</h2>";
+		}
+		else if ($op == "subtract")
+		{
+			echo "<h2>" . ($num1 - $num2) . "</h2>";
+		}
+		else if ($op == "multiply")
+		{
+			echo "<h2>" . ($num1 * $num2) . "</h2>";
+		}
+		else if ($op == "divide")
+		{
+			echo "<h2>" . ($num1 / $num2) . "</h2>";
+		}
+		else
+		{
+			echo "You haven't chosen an operation!";
+		}
 	?>
 
 	<br><br>
