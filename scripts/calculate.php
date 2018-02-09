6e00a304d827d2693225328385a5606061ce232b
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-09 13:31:52 +0300

    php test: everything work so far

---
+++ scripts/calculate.php
@@ -0,0 +1,12 @@
+<body>
+	Result: <br><br>
+
+	<?php
+		$username = $_POST['username'];
+		echo "Hello, " . $username;
+	?>
+
+	<br><br>
+
+	<a href = "../index.html">Go back to calculator</a>
+</body>
