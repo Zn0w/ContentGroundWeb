bb9e30493c8b8f8db71b667fb36c3c2e5bd5ddf0
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-13 19:50:05 +0300

    added html code to login.php

---
+++ forum/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ContentGroundForum Log in</title>
+	</head>
+
+	<body align = "center">
+		<form>
+			Your login: <br><br>
+			<input type = "text" name = "login">
+			<br><br><br><br>
+			Your password: <br><br>
+			<input type = "password" name = "password">
+			<br><br><br><br>
+			<input type = "submit" name = "submit" value = "Log in">
+		</form>
+	</body>
+</html>
