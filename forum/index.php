27262e04e681eec88b412ccc1e4dfa5fa409e993
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-13 19:35:37 +0300

    added login an register pages

--- forum/index.php
+++ forum/index.php
@@ -18,8 +18,12 @@
 		<?php
 			if (!isset($_COOKIE["userlogin"]))
 			{
-				echo "<p>Please, login in order to: 
-				create  content on this forum, starr discussions, enter communities an more!</p>";
+				echo "<p>Please, login or register in order to: 
+				create  content on this forum, start discussions, enter communities an more!</p>";
+
+				echo "<br>";
+
+				echo "<a href = 'login.php'>Log in</a> &nbsp; &nbsp; <a href = 'register.php'>Register</a>";
 			}
 			else
 			{
