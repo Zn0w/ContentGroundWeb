f02c5dc884eaffae94a7905ac17f32e210fb8da7
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-14 16:40:28 +0300

    added register html form

--- forum/register.php
+++ forum/register.php
@@ -6,6 +6,21 @@
 	</head>
 
 	<body>
-		
+		<form>
+			Your login: <br><br>
+			<input type = "text" name = "login">
+			<br><br><br><br>
+			Your name: <br><br>
+			<input type = "text" name = "name">
+			<br><br><br><br>
+			Your password: <br><br>
+			<input type = "password" name = "password">
+			<br><br><br><br>
+			<input type = "submit" name = "submit" value = "Register">
+		</form>
 	</body>
 </html>
+
+<?php
+	if ()
+?>
