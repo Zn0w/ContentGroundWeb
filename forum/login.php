6f133a85bc238ba0eb1091c8a96d6cc2e4a6d2a1
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-23 11:42:02 +0300

    working on login feature

--- forum/login.php
+++ forum/login.php
@@ -10,7 +10,7 @@
 	</head>
 
 	<body>
-		<form>
+		<form method = "post">
 			Your login: <br><br>
 			<input type = "text" name = "login">
 			<br><br><br><br>
@@ -30,5 +30,26 @@
 
 		$sql_get_user = "select * from users where login = '$user_login'";
 		$result = mysqli_query($db_connection, $sql_get_user);
+
+		/*while ($row = mysqli_fetch_assoc($result))
++		{
++			echo $row["login"] . "<br>";
++		}*/
+
+		if (!mysqli_num_rows($result))
+		{
+			echo "<h2>Sorry, a user with this login does not exist.</h2>";
+		}
+		else if (mysqli_fetch_assoc($result)["password"] != $user_password)
+		{
+			echo "<h2>Entered password is wrong.</h2>";
+		}
+		else
+		{
+			echo mysqli_num_rows($result);
+			$user_data = mysqli_fetch_row($result);
+			//echo "<h2>Welcome, " . $user_data["name"] . "</h2>";
+			echo $user_data[2];
+		}
 	}
 ?>
