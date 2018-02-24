5e47d4f45a0988219ff70a8c256d80ee4b247a62
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-24 20:00:39 +0300

    fixed login bug

--- forum/login.php
+++ forum/login.php
@@ -28,30 +28,24 @@
 		$user_login = $_POST["login"];
 		$user_password = $_POST["password"];
 
-		$sql_get_user = "select * from users";/* where login = '$user_login'";*/
+		$sql_get_user = "select * from users where login = '$user_login'";
 		$result = mysqli_query($db_connection, $sql_get_user);
 
-		while ($row = mysqli_fetch_assoc($result))
-+		{
-+			echo $row["login"] . "<br>";
-+		}
+		$user_data = mysqli_fetch_assoc($result);
 
-		if (!mysqli_num_rows($result))
+		if (!$user_data)
 		{
 			echo "<h2>Sorry, a user with this login does not exist.</h2>";
 		}
-		else if (mysqli_fetch_assoc($result)["password"] != $user_password)
+		else if ($user_data["password"] != $user_password)
 		{
 			echo "<h2>Entered password is wrong.</h2>";
 		}
 		else
 		{
-			echo mysqli_num_rows($result);
-			$user_data = mysqli_fetch_assoc($result);
-			
 			echo $user_data["name"];
 		}
 
-		mysli_close($db_connection);
+		//mysli_close($db_connection);
 	}
 ?>
