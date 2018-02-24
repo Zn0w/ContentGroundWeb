23b28987336b1b020805a28309f57b9554816fce
Daniil Blagiy <blagiy.daniil@gmail.com>
2018-02-24 20:33:24 +0300

    defined cookies setup function

--- forum/includes/userdata_cookies.php
+++ forum/includes/userdata_cookies.php
@@ -2,5 +2,6 @@
 
 function init_user_cookies($user_login, $user_name)
 {
-	
+	setcookie("user_login", $user_login, time() + (86400 * 30), "/");
+	setcookie("user_name", $user_name, time() + (86400 * 30), "/");
 }
